a468b4c9665fed3adb548e74e7b03673cc404bae
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-07-18 19:20:23 -0400

    :arrow_up: Install neat-csv package

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -2,11 +2,13 @@
 import fs from 'fs';
 import csv from 'csv-parse';
 import path from 'path';
+import neatCsv from 'neat-csv';
 
 import uploadConfig from '../config/upload';
 
 import Transaction from '../models/Transaction';
 import CreateTransactionService from './CreateTransactionService';
+import parse from 'csv-parse';
 
 interface TransactionCSV {
   title: string;
@@ -17,29 +19,36 @@ interface TransactionCSV {
 
 class ImportTransactionsService {
   public async execute(filename: string): Promise<Transaction[]> {
-    const createTransactionService = new CreateTransactionService();
+    // const createTransactionService = new CreateTransactionService();
     const transactions: Transaction[] = [];
-    const importedCSVFile: TransactionCSV[] = [];
+    // const importedCSVFile: TransactionCSV[] = [];
     const filePath = path.join(uploadConfig.directory, filename);
 
-    fs.createReadStream(filePath)
-      .pipe(csv({ columns: true }))
-      .on('data', (data: TransactionCSV) => importedCSVFile.push(data))
-      .on('end', () => {
-        importedCSVFile.map(async csvData => {
-          const { title, category, type, value } = csvData;
-          const transaction = await createTransactionService.execute({
-            title,
-            category,
-            type,
-            value,
-          });
-          transactions.push(transaction);
-        });
-        console.log(transactions);
-      });
+    fs.readFile(filePath, async (error, data) => {
+      await neatCsv(data).then((parsedData) => console.log(parsedData));
+    })
 
-    console.log(transactions);
+    // await fs.createReadStream(filePath)
+    //   .pipe(csv({ columns: true }))
+    //   .on('data', (row: TransactionCSV) => importedCSVFile.push(row))
+    //   .on('end', () => {
+    //     importedCSVFile.map(async csvRow => {
+    //       const { title, category, type, value } = csvRow;
+
+    //       // console.log(title);
+
+    //       const transaction = await createTransactionService.execute({
+    //         title,
+    //         category,
+    //         type,
+    //         value,
+    //       });
+
+    //       transactions.push(transaction);
+
+    //       console.log(transaction);
+    //     });
+    //   });
 
     return transactions;
   }
