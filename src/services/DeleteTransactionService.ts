960d9f2dcc0bb1f0d08593069a7fd7f20c9c48c5
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-07-17 21:17:53 -0400

    :rocket: Deleting route is finished

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+/* eslint-disable prettier/prettier */
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const deletedTransaction = await transactionsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!deletedTransaction) {
+      throw new AppError('Transaction not found', 404);
+    }
+
+    await transactionsRepository.remove(deletedTransaction);
   }
 }
 
